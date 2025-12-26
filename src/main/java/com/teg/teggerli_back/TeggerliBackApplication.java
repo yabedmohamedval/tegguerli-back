b5459c66cc72a9d9737c58fdaf0b25d7f15ed4b1
yabedmohamedval <yabedmohamedval61@gmail.com>
2025-12-26 21:07:53 +0100

    Initial commit

---
+++ src/main/java/com/teg/teggerli_back/TeggerliBackApplication.java
@@ -0,0 +1,13 @@
+package com.teg.teggerli_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeggerliBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeggerliBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teg/teggerli_back/TeggerliBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teg.teggerli_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeggerliBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
